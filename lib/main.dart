dcbe1e4e08986eb7aaa623fb27045a79bca709b1
DiegoMolina65 <diegobrian042@gmail.com>
2023-10-27 01:05:40 -0400

    home

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:reproductor/artista.dart';
 
-var kColorScheme = ColorScheme.fromSeed(
-  seedColor: const Color.fromARGB(255, 10, 1, 29),
-);
-
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData().copyWith(
-          colorScheme: kColorScheme,
-          useMaterial3: true,
-          appBarTheme: const AppBarTheme(
-            iconTheme: IconThemeData(color: Colors.white),
-          )),
       home: const Artista(),
     );
   }
